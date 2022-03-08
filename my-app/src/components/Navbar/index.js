40b9d220fb066a8deb6bb955f0f8cdfac66228b7
Arcmazter <aronlex@gmail.com>
2022-03-08 18:08:03 +0000

    Logo

--- my-app/src/components/Navbar/index.js
+++ my-app/src/components/Navbar/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import styles from './styles.css';
+import logo from '../img/bubbleLogo.png';
 
 const Navbar = () => {
     return (
         <>
         <div className='nav-links' style={styles}>
-        <img src="img/bubbleLogo.jpg" alt="BubblyLogo"></img>
+        <img src={logo} alt="BubblyLogo"></img>
         <ul >
             <li >
                 <Link to="/">Products</Link>
