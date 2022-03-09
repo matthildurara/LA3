593e79a693cdc5fee5413b1615fe06d32f4fabd2
berthamaria <54938563+berthamaria@users.noreply.github.com>
2022-03-09 13:41:21 +0000

    about

--- my-app/src/components/about/index.js
+++ my-app/src/components/about/index.js
@@ -9,6 +9,7 @@ const About = () => {
         <div className='container'>
             <h1 className='aboutHeading'>About Bubblify </h1>
             <p> Bubblify is a comapany in the bubble industry</p>
+            <p><strong>Address:</strong> Bubble Street 88, Iceland </p>
             <div className='contactBubbly'>
                 <h1 className='contactUs'>Contact us</h1>
                 <p><strong>Phone:</strong> 8888888</p>
