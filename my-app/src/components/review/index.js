a2d86e90fdf0abac6e4af126234f086fa950b10f
Matthildur <matthildura19@ru.is>
2022-03-12 11:44:32 +0000

    save orders

--- my-app/src/components/review/index.js
+++ my-app/src/components/review/index.js
@@ -3,6 +3,7 @@ import { getCart } from '../../services/cartService';
 import ReviewView from '../reviewView';
 import styles from './styles.css';
 import { useLocation } from "react-router-dom";
+import { saveOrder } from '../../services/orderService';
 
 
 const Review = () => {
@@ -10,18 +11,25 @@ const Review = () => {
     console.log(location);
      const {customer} = location.state;
     console.log(`customer: ${customer}`);
+    const[order,setOrder] = useState({})
     // console.log(`the form is: ${formin.state}`);
     // const {name,phone} = (location.state.customerPickup);
     // console.log(name);
     //  console.log(`formin: ${location.state}`);
     // const [customer,setcustomer] = useState({});
-    let cust = localStorage.getItem('customer');
+    // let cust = localStorage.getItem('customer');
     var cart = getCart();
     // setcustomer(cust);
     // let items = JSON.parse(localStorage.getItem('item'));
-    console.log(cust);
-    const handleConfirm = () => {
-        
+    // console.log(cust);
+    const handleConfirm = (cart,customer) => {
+        // const [order,setOrder] = useState({});
+        let order = {}
+        const phone = customer.phone;
+        order['cart'] = cart;
+        order['customer'] = customer;
+        console.log(order);
+        saveOrder(order,phone);
     }
     return (
         <div >
@@ -43,7 +51,9 @@ const Review = () => {
                  <ReviewView key={index} item={item} />
                             ))} 
                             </div>
-            <div> Confirm</div>
+            <div onClick={ () => handleConfirm(cart,customer) }className="confirm" > Confirm</div>
+         {/* <div onClick={ () => addToCart(item) }className="remove-cart" style={styles}> Remove from cart</div> */}
+
 
 
         </div>
--- my-app/src/services/orderService/index.js
+++ my-app/src/services/orderService/index.js
@@ -1,12 +1,20 @@
-export const saveOrder = (order,customer) => {
-    const [order,setorder] = useState({
-        customer:{
-        name: "",
-        address: "",
-        phone: "",
-        city: "",
-        code: ""
-        }
-    });
+import React, {useState} from 'react';
+export const saveOrder = async (order,phone) => {
+    const url = 'http://localhost:3500/api/orders/' + phone; 
+    await (fetch(url,{
+        method:'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({order})
+    }))
+    console.log('Order saved!');
+    // console.log(order);
+    // requestOptions = {
+    //     method: 'POST',
+    //     headers: { 'Content-Type': 'application/json' },
+    //     body: JSON.stringify({ order})
+    // };
+    // fetch('https://reqres.in/api/posts', requestOptions)
+    //     .then(response => response.json())
+    //     .then(data => setPostId(data.id));
 
 };
